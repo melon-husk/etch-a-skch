822e1dbec895b4700fdd1ba950ed0671433a1d09
Ujwal Bhagat <ujwal5555@gmail.com>
2020-05-07 22:36:53 +0530

    added divArray

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = i+1;
+        container.appendChild(cell).className = "grid-item";
+        container.appendChild(cell).id = i;
+    };
+    
+};
+makeGrid(8, 8);
+let divArray = document.getElementById('container').children;
+
+for(let i of divArray) {
+    
+    i.addEventListener('mouseover', function () {
+        black(i);
+    });
+}
+
+
+function black(i) {
+    i.style.backgroundColor="black";
+    console.log('in black')
+}
+console.log("it runs");
